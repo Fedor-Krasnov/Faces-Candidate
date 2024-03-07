ad44073828b445f6e21acb9fe0fe2a768b236f9d
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-07 12:08:59 +0300

    feat(Footer): Добавление шаблона Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import './Footer.scss';
+import { footerData } from '../../mocks/components/footerData';
 
-const Footer = () => <div>Footer</div>;
+const footerClassName = 'footer';
+
+const Footer = () => {
+  const {} = footerData;
+
+  return (
+    <footer className={footerClassName}>
+      <div className={`${footerClassName}__main-information`}>
+        <div className={`${footerClassName}__candidates`}></div>
+        <div className={`${footerClassName}__work`}></div>
+        <div className={`${footerClassName}__help`}></div>
+      </div>
+    </footer>
+  );
+};
 
 export { Footer };
