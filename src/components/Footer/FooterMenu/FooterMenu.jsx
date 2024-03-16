374d9b76b61cc60f5c0da53d15606a456236a291
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-16 18:53:26 +0300

    chore(FooterMenu): CSS-module

--- src/components/Footer/FooterMenu/FooterMenu.jsx
+++ src/components/Footer/FooterMenu/FooterMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './FooterMenu.scss';
 import { Link } from 'react-router-dom';
 import { footerData } from '../../../mocks/components/footerData';
+import footerMenuStyles from './FooterMenu.module.scss';
 
 const footerMenuClassName = 'footer-menu';
 
@@ -9,11 +9,11 @@ const FooterMenu = () => {
   const { footerMenu } = footerData;
 
   return (
-    <ul className={footerMenuClassName}>
+    <ul className={footerMenuStyles[footerMenuClassName]}>
       {footerMenu.map(({ title, menu }, footerMenuIndex) => (
         <li key={footerMenuIndex}>
           <strong dangerouslySetInnerHTML={{ __html: title }} />
-          <ul className={`${footerMenuClassName}__menu`}>
+          <ul className={footerMenuStyles[`${footerMenuClassName}__menu`]}>
             {menu.map(({ link, text }, indexMenu) => (
               <li key={indexMenu}>
                 <Link dangerouslySetInnerHTML={{ __html: text }} to={link} />
