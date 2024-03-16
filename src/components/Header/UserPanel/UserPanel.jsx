1beb1824137cba0b53e5a47e9c3e57abbbed4e3f
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-16 18:45:40 +0300

    chore(UserPanel): CSS-module

--- src/components/Header/UserPanel/UserPanel.jsx
+++ src/components/Header/UserPanel/UserPanel.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './UserPanel.scss';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { headerData } from '../../../mocks';
 import { Button, Icon } from '../../units';
 import { IconCode } from '../../units/Icon';
+import userPanelStyles from './UserPanel.module.scss';
 
 const userPanelClassName = 'user-panel';
 const user = 'Fedor Krasnov';
@@ -18,21 +18,25 @@ const UserPanel = ({ isLogged }) => {
   };
 
   return (
-    <div className={classNames(userPanelClassName, { [`${userPanelClassName}__is-logged`]: isLogged })}>
+    <div
+      className={classNames(userPanelStyles[userPanelClassName], {
+        [userPanelStyles[`${userPanelClassName}__is-logged`]]: isLogged,
+      })}
+    >
       {isLogged ? (
         <>
-          <div className={`${userPanelClassName}__user`}>
+          <div className={userPanelStyles[`${userPanelClassName}__user`]}>
             <span>{getAbbrUser(user)}</span>
             <span>{user}</span>
           </div>
-          <div className={`${userPanelClassName}__popup`}>
-            <Link className={`${userPanelClassName}__popup-content`} to={linkModalProfile}>
+          <div className={userPanelStyles[`${userPanelClassName}__popup`]}>
+            <Link className={userPanelStyles[`${userPanelClassName}__popup-content`]} to={linkModalProfile}>
               <p>Посмотреть профиль</p>
               <Icon code={IconCode.user} />
             </Link>
-            <Link className={`${userPanelClassName}__popup-content`} to={linkModalExit}>
+            <Link className={userPanelStyles[`${userPanelClassName}__popup-content`]} to={linkModalExit}>
               <span>Выйти</span>
-              <Icon className={`${userPanelClassName}__icon-exit`} code={IconCode.exit} />
+              <Icon className={userPanelStyles[`${userPanelClassName}__icon-exit`]} code={IconCode.exit} />
             </Link>
           </div>
         </>
