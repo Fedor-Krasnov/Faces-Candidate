e7346f4537cd786eb1f75cab7ce03f03dfbcb2ca
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-25 12:05:34 +0300

    chore(DirectionOfWork): CSS-module

--- src/components/sections/DirectionOfWork/DirectionOfWork.jsx
+++ src/components/sections/DirectionOfWork/DirectionOfWork.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './DirectionOfWork.scss';
 import { Link } from 'react-router-dom';
 import { directionOfWorkData } from '../../../mocks/sections';
 import { Icon, Title } from '../../units';
+import directionOfWorkStyles from './DirectionOfWork.module.scss';
 
 const directionOfWorkClassName = 'direction-of-work';
 
@@ -10,11 +10,11 @@ const DirectionOfWork = () => {
   const { title, options } = directionOfWorkData;
 
   return (
-    <section className={directionOfWorkClassName}>
+    <section className={directionOfWorkStyles[directionOfWorkClassName]}>
       <Title title={title} titleLevel="h2" />
-      <div className={`${directionOfWorkClassName}__container`}>
+      <div className={directionOfWorkStyles[`${directionOfWorkClassName}__container`]}>
         {options.map(({ icon, optionsTitle }, optionIndex) => (
-          <Link key={optionIndex} className={`${directionOfWorkClassName}__options`} to="tmp">
+          <Link key={optionIndex} className={directionOfWorkStyles[`${directionOfWorkClassName}__options`]} to="tmp">
             <Icon code={icon} />
             <div dangerouslySetInnerHTML={{ __html: optionsTitle }} />
           </Link>
