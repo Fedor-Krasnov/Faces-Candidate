461d220949d8134ec850a8daaf856a46e9781b80
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-25 11:57:10 +0300

    chore(Advantages): CSS-module

--- src/components/sections/Advantages/Advantages.jsx
+++ src/components/sections/Advantages/Advantages.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './Advantages.scss';
 import { advantagesData } from '../../../mocks/sections';
 import { Button, Icon, Title } from '../../units';
+import advantagesStyles from './Advantages.module.scss';
 
 const advantagesClassName = 'advantages';
 
@@ -9,32 +9,32 @@ const Advantages = () => {
   const { titleLeftBlock, titleRightBlock, descriptionRightBlock, buttonTitle, dataPrice, points } = advantagesData;
 
   return (
-    <section className={advantagesClassName}>
-      <div className={`${advantagesClassName}__left-block`}>
+    <section className={advantagesStyles[advantagesClassName]}>
+      <div className={advantagesStyles[`${advantagesClassName}__left-block`]}>
         <Title title={titleLeftBlock} titleLevel="h2" />
-        <div className={`${advantagesClassName}__points`}>
+        <div className={advantagesStyles[`${advantagesClassName}__points`]}>
           {points.map(({ pointsTitle, pointsDescription, icon }, pointsIndex) => (
             <div key={pointsIndex}>
-              <div className={`${advantagesClassName}__point`}>
+              <div className={advantagesStyles[`${advantagesClassName}__point`]}>
                 <Icon code={icon} />
                 <Title title={pointsTitle} titleLevel="h3" />
               </div>
               <p
-                className={`${advantagesClassName}__points-description`}
+                className={advantagesStyles[`${advantagesClassName}__points-description`]}
                 dangerouslySetInnerHTML={{ __html: pointsDescription }}
               />
             </div>
           ))}
         </div>
       </div>
-      <div className={`${advantagesClassName}__right-block`} data-price={dataPrice}>
+      <div className={advantagesStyles[`${advantagesClassName}__right-block`]} data-price={dataPrice}>
         <Title
-          className={`${advantagesClassName}__title`}
+          className={advantagesStyles[`${advantagesClassName}__title`]}
           description={descriptionRightBlock}
           title={titleRightBlock}
           titleLevel="h2"
         />
-        <Button className={`${advantagesClassName}__button`} secondary title={buttonTitle} />
+        <Button className={advantagesStyles[`${advantagesClassName}__button`]} secondary title={buttonTitle} />
       </div>
     </section>
   );
