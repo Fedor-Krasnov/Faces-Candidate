349541b4c75eea651ac8d6f9188f3f645c521f55
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-04-11 09:46:48 +0300

    feat(SubscribeToTheDatabase): Добавление десктопной версии секции

--- src/components/sections/SubscribeToTheDatabase/SubscribeToTheDatabase.jsx
+++ src/components/sections/SubscribeToTheDatabase/SubscribeToTheDatabase.jsx
@@ -14,15 +14,26 @@ const SubscribeToTheDatabase = () => {
         <img alt={image.alt} src={image.src} />
       </div>
       <div className={subscribeToTheDatabaseStyles[`${subscribeToTheDatabaseClassName}__right-block-content`]}>
-        <Title description={description} title={title} />
-        {questionnaire.map(({ placeholder }, inputIndex) => (
-          <Input key={inputIndex} placeholder={placeholder} />
-        ))}
-        <Button title={buttonTitle} />
+        <div
+          className={subscribeToTheDatabaseStyles[`${subscribeToTheDatabaseClassName}__right-block-content-container`]}
+        >
+          <Title
+            className={subscribeToTheDatabaseStyles[`${subscribeToTheDatabaseClassName}__title`]}
+            description={description}
+            title={title}
+            titleLevel="h2"
+          />
+          <div className={subscribeToTheDatabaseStyles[`${subscribeToTheDatabaseClassName}__questionnaire`]}>
+            {questionnaire.map(({ placeholder }, inputIndex) => (
+              <Input key={inputIndex} placeholder={placeholder} />
+            ))}
+          </div>
+          <Button title={buttonTitle} width="fill" />
+        </div>
         <div className={subscribeToTheDatabaseStyles[`${subscribeToTheDatabaseClassName}__warning`]}>
           <p>
-            Отправка данной формы означает согласие с <a>Пользовательским соглашением</a> и{' '}
-            <a>Политикой конфиденциальности</a>
+            Отправка данной формы означает согласие с <a href="#">Пользовательским соглашением</a> и{' '}
+            <a href="#">Политикой конфиденциальности</a>
           </p>
         </div>
       </div>
