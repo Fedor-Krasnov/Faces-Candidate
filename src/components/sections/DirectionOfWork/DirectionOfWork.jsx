c4e74cc5af2c51d60efe53525ef9a70d5f67f6a4
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-05-05 00:29:09 +0300

    feat(MouseMoveContainer): Создание контейнера для свайпа

--- src/components/sections/DirectionOfWork/DirectionOfWork.jsx
+++ src/components/sections/DirectionOfWork/DirectionOfWork.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Icon, Title } from '../../units';
+import { Icon, MouseMoveContainer, Title } from '../../units';
 import directionOfWorkStyles from './DirectionOfWork.module.scss';
 
 const directionOfWorkClassName = 'direction-of-work';
@@ -12,25 +12,33 @@ const DirectionOfWork = ({ content }) => {
     <div className={directionOfWorkStyles[directionOfWorkClassName]}>
       <Title title={title} titleLevel="h2" />
       <div className={directionOfWorkStyles[`${directionOfWorkClassName}__container`]}>
-        {options.map(({ icon, optionsTitle, count }, optionIndex) => {
-          const modificationCount = count >= 100 ? '99+' : count;
+        <MouseMoveContainer>
+          <div className={directionOfWorkStyles[`${directionOfWorkClassName}__content`]}>
+            {options.map(({ icon, optionsTitle, count }, optionIndex) => {
+              const modificationCount = count >= 100 ? '99+' : count;
 
-          return (
-            <Link key={optionIndex} className={directionOfWorkStyles[`${directionOfWorkClassName}__options`]} to="tmp">
-              <div className={directionOfWorkStyles[`${directionOfWorkClassName}__icon`]}>
-                <Icon code={icon} />
-              </div>
-              <div
-                className={directionOfWorkStyles[`${directionOfWorkClassName}__option-title`]}
-                dangerouslySetInnerHTML={{ __html: optionsTitle }}
-              />
-              <div
-                className={directionOfWorkStyles[`${directionOfWorkClassName}__count`]}
-                dangerouslySetInnerHTML={{ __html: modificationCount }}
-              />
-            </Link>
-          );
-        })}
+              return (
+                <Link
+                  key={optionIndex}
+                  className={directionOfWorkStyles[`${directionOfWorkClassName}__options`]}
+                  to="tmp"
+                >
+                  <div className={directionOfWorkStyles[`${directionOfWorkClassName}__icon`]}>
+                    <Icon code={icon} />
+                  </div>
+                  <div
+                    className={directionOfWorkStyles[`${directionOfWorkClassName}__option-title`]}
+                    dangerouslySetInnerHTML={{ __html: optionsTitle }}
+                  />
+                  <div
+                    className={directionOfWorkStyles[`${directionOfWorkClassName}__count`]}
+                    dangerouslySetInnerHTML={{ __html: modificationCount }}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        </MouseMoveContainer>
       </div>
     </div>
   );
--- src/components/units/index.js
+++ src/components/units/index.js
@@ -2,5 +2,6 @@ export { Button } from './Button';
 export { Icon } from './Icon';
 export { Input } from './Input';
 export { Logo } from './Logo';
+export { MouseMoveContainer } from './MouseMoveContainer';
 export { Title } from './Title';
 export { UseOfCookies } from './UseOfCookies';
