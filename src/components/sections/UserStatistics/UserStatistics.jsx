41a21a3b46b3de0e6187a70b9ce7b9fe1245a4ee
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-05-22 16:58:42 +0300

    feat(UserStatistics): Добавление контента в секцию UserStatistics

--- src/components/sections/UserStatistics/UserStatistics.jsx
+++ src/components/sections/UserStatistics/UserStatistics.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { userProfileData } from '../../../mocks/sections';
+import { Title } from '../../units';
 import userStatisticsStyles from './UserStatistics.module.scss';
 
 const userStatisticsClassName = 'user-statistics';
 
-const UserStatistics = () => <div className={userStatisticsStyles[userStatisticsClassName]}></div>;
+const UserStatistics = () => {
+  const {
+    titleStatistic,
+    titleButtonContact,
+    currentAmountSubscribe,
+    numberLimitSubscribe,
+    titleCandidates,
+    numberLimitCandidates,
+    currentAmountCandidates,
+    titleButtonCandidate,
+  } = userProfileData;
+
+  return (
+    <section className={userStatisticsStyles[userStatisticsClassName]}>
+      <div className={userStatisticsStyles[`${userStatisticsClassName}__container`]}>
+        <div className={userStatisticsStyles[`${userStatisticsClassName}__contacts`]}>
+          <Title title={titleStatistic} titleLevel="h4" />
+          <div className={userStatisticsStyles[`${userStatisticsClassName}__contacts-container`]}>
+            <div>
+              {currentAmountSubscribe}/{numberLimitSubscribe}
+            </div>
+            <Link to="tmp">
+              <div dangerouslySetInnerHTML={{ __html: titleButtonContact }} />
+            </Link>
+          </div>
+        </div>
+        <div className={userStatisticsStyles[`${userStatisticsClassName}__candidates`]}>
+          <Title title={titleCandidates} titleLevel="h4" />
+          <div className={userStatisticsStyles[`${userStatisticsClassName}__candidates-container`]}>
+            <div>
+              {currentAmountCandidates}/{numberLimitCandidates}
+            </div>
+            <Link to="tmp">
+              <div dangerouslySetInnerHTML={{ __html: titleButtonCandidate }} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export { UserStatistics };
--- src/mocks/sections/userProfileData.js
+++ src/mocks/sections/userProfileData.js
@@ -1,4 +1,12 @@
 export const userProfileData = {
+  currentAmountSubscribe: '0',
+  currentAmountCandidates: '0',
+  numberLimitSubscribe: '50',
+  numberLimitCandidates: '100',
+  titleCandidates: 'Посмотрено кандидатов:',
+  titleStatistic: 'Открыто контактов:',
+  titleButtonContact: 'Докупить контакты',
+  titleButtonCandidate: 'Скрыть просмотренных',
   buttonTitle: 'Оформить',
   titleSubscribe: 'Оформить подписку',
   subscriptionContent: [
