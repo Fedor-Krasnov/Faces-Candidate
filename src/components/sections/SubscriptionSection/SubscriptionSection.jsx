c1fec9eaf34e00d8fd3fc373f09cefba0b4dd27f
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-05-22 09:42:28 +0300

    refactor(UserProfile/SubscriptionSection): Перенос секции SubscriptionSection в отдельный файл

---
+++ src/components/sections/SubscriptionSection/SubscriptionSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { userProfileData } from '../../../mocks/sections';
+import { Button, Title } from '../../units';
+import subscriptionSectionStyles from './SubscriptionSection.module.scss';
+
+const subscriptionSectionClassName = 'subscription-section';
+
+const SubscriptionSection = () => {
+  const { title, buttonTitle, subscriptionContent } = userProfileData;
+
+  return (
+    <section className={subscriptionSectionStyles[subscriptionSectionClassName]}>
+      <Title
+        className={subscriptionSectionStyles[`${subscriptionSectionClassName}__subscription-title`]}
+        title={title}
+        titleLevel="h4"
+      />
+      <div className={subscriptionSectionStyles[`${subscriptionSectionClassName}__subscription-content`]}>
+        {subscriptionContent.map(({ validity, price }, subIndex) => (
+          <div
+            key={subIndex}
+            className={subscriptionSectionStyles[`${subscriptionSectionClassName}__subscription-offers`]}
+          >
+            <div>
+              <p dangerouslySetInnerHTML={{ __html: validity }} />
+              <p dangerouslySetInnerHTML={{ __html: price }} />
+            </div>
+            <Button title={buttonTitle} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export { SubscriptionSection };
